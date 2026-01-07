49282255f8d03e545b33fcbf6294da74ba591734
fredcoq <frederik.coquelet@gmail.com>
2026-01-07 09:53:51 +0100

    ok

--- scripts/start-kubevm.sh
+++ scripts/start-kubevm.sh
@@ -1,11 +1,14 @@
 #! /bin/bash
 
-kubectl get configmap kube-proxy -n kube-system -o yaml | \
-sed -e "s/strictARP: false/strictARP: true/" | \
-kubectl apply -f - -n kube-system
+#kubectl get configmap kube-proxy -n kube-system -o yaml | \
+#sed -e "s/strictARP: false/strictARP: true/" | \
+#kubectl apply -f - -n kube-system
 
-kustomize build --enable-helm ../openebs | kubectl apply -f -
-#kustomize build ../sealed-secrets | kubectl apply -f -
-#sleep 5
-#kustomize build ../argocd | kubectl apply -f -
+#kustomize build --enable-helm ../openebs | kubectl apply -f -
 
+kustomize build ../sealed-secrets | kubectl apply -f -
+# kubectl create namespace rook-ceph
+kustomize build --enable-helm ../rook-ceph | kubectl apply -n rook-ceph --server-side=true -f -
+sleep 10
+kustomize build --enable-helm ../rook-ceph | kubectl apply -n rook-ceph --server-side=true -f -
+kustomize build --enable-helm ../argocd | kubectl apply --server-side=true -f -
